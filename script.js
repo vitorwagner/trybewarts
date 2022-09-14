ff4a837a65ca3a4a59067e7ddf74d433895ce454
uelionobre <uelionobre@hotmail.com>
2022-09-14 17:42:36 -0300

    adicao de estilos no header e formulario de login

---
+++ script.js
@@ -0,0 +1,17 @@
+// Valida o formulário do header de acordo como o requisito 3
+function validarFormularioLogin(e) {
+  e.preventDefault();
+
+  const email = e.target[0].value;
+  const password = e.target[1].value;
+  const isTrybeEmailTest = email === 'tryber@teste.com' && password === '123456';
+
+  if (isTrybeEmailTest) {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+const formHeader = document.querySelector('.trybewarts-login');
+formHeader.addEventListener('submit', validarFormularioLogin);
